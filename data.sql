5815652ab363ffd3d1520f6b7cf760592cf0b004
Domantas Alenskas <domantas.alenskas@gmail.com>
2017-12-14 23:02:39 +0200

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,9 +1,72 @@
-CREATE TABLE users(
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 14, 2017 at 07:18 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
 
-  fname VARCHAR (30),
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-  lname VARCHAR (30),
 
-  passwd VARCHAR (30)
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-);
+--
+-- Database: `hospital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedules`
+--
+
+CREATE TABLE `schedules` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `timeStart` time NOT NULL,
+  `timeEnd` time NOT NULL,
+  `workDays` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `schedules`
+--
+
+INSERT INTO `schedules` (`id`, `fname`, `lname`, `timeStart`, `timeEnd`, `workDays`) VALUES
+(1, 'Domantas', 'Alenskas', '10:00:00', '15:00:00', 'I-V'),
+(3, 'Angele', 'Angelyte', '11:37:00', '19:31:00', 'I, II, IV, VII'),
+(4, 'Angele', 'Angelyte', '11:37:00', '19:31:00', 'I, II, IV, VII');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `schedules`
+--
+ALTER TABLE `schedules`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `schedules`
+--
+ALTER TABLE `schedules`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
