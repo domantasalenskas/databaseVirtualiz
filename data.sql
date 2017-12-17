455b9c2e8ca125e10393fd33b571e9b8e85d7753
Domantas Alenskas <domantas.alenskas@gmail.com>
2017-12-17 23:05:44 +0200

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 14, 2017 at 07:18 PM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.1.11
+-- Host: localhost
+-- Generation Time: Dec 17, 2017 at 11:03 PM
+-- Server version: 5.7.20-0ubuntu0.16.04.1
+-- PHP Version: 7.0.22-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -25,48 +23,146 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `schedules`
+-- Table structure for table `appointments`
 --
 
-CREATE TABLE `schedules` (
+CREATE TABLE `appointments` (
   `id` int(11) NOT NULL,
-  `fname` varchar(255) NOT NULL,
-  `lname` varchar(255) NOT NULL,
-  `timeStart` time NOT NULL,
-  `timeEnd` time NOT NULL,
-  `workDays` varchar(255) NOT NULL
+  `doctor_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `time` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `schedules`
+-- Dumping data for table `appointments`
 --
 
-INSERT INTO `schedules` (`id`, `fname`, `lname`, `timeStart`, `timeEnd`, `workDays`) VALUES
-(1, 'Domantas', 'Alenskas', '10:00:00', '15:00:00', 'I-V'),
-(3, 'Angele', 'Angelyte', '11:37:00', '19:31:00', 'I, II, IV, VII'),
-(4, 'Angele', 'Angelyte', '11:37:00', '19:31:00', 'I, II, IV, VII');
+INSERT INTO `appointments` (`id`, `doctor_id`, `user_id`, `time`) VALUES
+(93, 1, 3, 1513490400),
+(94, 1, 3, 1513490400),
+(95, 5, 3, 1513490400),
+(96, 1, 3, 1513494000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctors`
+--
+
+CREATE TABLE `doctors` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `profession` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctors`
+--
+
+INSERT INTO `doctors` (`id`, `first_name`, `last_name`, `profession`) VALUES
+(1, 'Tomas', 'Macikas', 'Psichologas'),
+(2, 'Deividas', 'Miliauskas', 'Chirurgas'),
+(3, 'Irmantas', 'Radavicius', 'Chirurgas'),
+(4, 'Andrius', 'Butkevicius', 'Psichologas'),
+(5, 'ruta', 'rutela', 'administratore');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `professions`
+--
+
+CREATE TABLE `professions` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `professions`
+--
+
+INSERT INTO `professions` (`id`, `title`) VALUES
+(1, 'Psichologas'),
+(2, 'Chirurgas');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `snumber` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `lastname`, `password`, `email`, `snumber`) VALUES
+(1, 'Miliauskas', 'voras', 'test@test.lt', 11254),
+(3, 'juozas', 'voras', 'dmz@test.lt', 132154),
+(4, 'simonas', 'symka', 'test@testas.lt', 11111);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `schedules`
+-- Indexes for table `appointments`
 --
-ALTER TABLE `schedules`
+ALTER TABLE `appointments`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `doctors`
+--
+ALTER TABLE `doctors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `professions`
+--
+ALTER TABLE `professions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `snumber` (`snumber`),
+  ADD UNIQUE KEY `snumber_2` (`snumber`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `schedules`
+-- AUTO_INCREMENT for table `appointments`
 --
-ALTER TABLE `schedules`
+ALTER TABLE `appointments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
+--
+-- AUTO_INCREMENT for table `doctors`
+--
+ALTER TABLE `doctors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `professions`
+--
+ALTER TABLE `professions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
